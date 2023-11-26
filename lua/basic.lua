2a9f5573b464454f81b88f9147fdd1f15260a147
Dane Walters <dwalters.maruko@gmail.com>
2023-11-26 17:05:38 +0800

    feat: bracket auto completion

--- lua/basic.lua
+++ lua/basic.lua
@@ -3,3 +3,25 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.o.clipboard = "unnamedplus"
+
+
+vim.cmd [[
+  augroup LuaSettings
+    autocmd!
+    autocmd FileType lua setlocal tabstop=2 shiftwidth=2 expandtab softtabstop=2
+  augroup END
+]]
+
+vim.cmd([[
+  augroup TypeScriptSettings
+    autocmd!
+    autocmd FileType typescript setlocal tabstop=2 shiftwidth=2 expandtab softtabstop=2
+  augroup END
+]])
+
+vim.cmd([[
+  augroup CSettings
+    autocmd!
+    autocmd FileType c setlocal tabstop=4 shiftwidth=4 expandtab softtabstop=4
+  augroup END
+]])
---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+local M = {
+  "windwp/nvim-autopairs",
+}
+
+M.event = "InsertEnter"
+M.opts = {}
+
+return M
