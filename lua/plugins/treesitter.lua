fdf4be9e65493439d4829c0dfac9f30924555a4a
Dane Walters <dwalters.maruko@gmail.com>
2023-11-26 16:18:06 +0800

    feat: nvim treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+local M = {
+  'nvim-treesitter/nvim-treesitter',
+}
+
+M.build = function()
+  require('nvim-treesitter.install').update({ with_sync = true })()
+end
+
+M.config = function()
+  local configs = require('nvim-treesitter.configs')
+
+  configs.setup({
+    ensure_installed = { 'rust', 'lua', 'typescript', 'javascript', 'elixir', 'c' },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        node_decremental = "<BS>",
+        scope_incremental = "<TAB>",
+      },
+    },
+  })
+end
+
+return M
